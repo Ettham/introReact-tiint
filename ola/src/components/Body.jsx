9349c079b56c24351bf269d597337fb729f38c8b
ettham <etthamartinez24@gmail.com>
2025-05-26 22:20:23 -0300

    Descrição do commit

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dinos são legais</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam illum voluptatibus tempore, temporibus facere provident sit! Rerum odit, voluptatem sed cupiditate beatae, dolorum perspiciatis, ullam nesciunt aliquid quae sequi natus!</p>
+    </div>
+  )
+}
+
+export default Body
