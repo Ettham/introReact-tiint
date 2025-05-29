001fb05c8f50f9ca0a72fe1a949181d96e461dfc
ettham <etthamartinez24@gmail.com>
2025-05-28 21:56:25 -0300

    terminamos la pagina

--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,72 +1,32 @@
-import { useState } from 'react'
 import './Body.css'
+function Body(props) {
 
-function Body() {
-  const[cont, setCont] = useState(0)
-  const[inputValor, setinputValor] = useState('')
-  const[inputN1, setInputN1] = useState('')
-  const[inputN2, setInputN2] = useState('')
-
-  function aumentar(){
-    setCont(cont + 1)
-  }
-
-  function desminuir(){
-    if( cont > 0){
-      setCont(cont - 1)
-    }
-  }
-
-    function alterar(){
-      setCont(Number(inputValor))
-    }
-
-    function somas(){
-      setCont(Number(inputN1) + Number(inputN2))
-    }
-
-    function restas(){
-      setCont(inputN1 - inputN2)
-    }
-
-    function multiplicacao (){
-      setCont(inputN1 * inputN2)
-    }
 
   return (
     <div className='Body'>
-        <h2 className='titulo2'>Dinos são legais</h2>
-        <div className='cont'>
-        <button onClick={desminuir}>-</button>
-        <p>contagem: {cont}</p>
-       <button onClick={aumentar}>+</button>
-        </div>
+      <div className='grid'>
 
-        <div className="from">
-          <input type="number"
-          value={inputValor}
-          onChange={(e) => setinputValor(e.target.value)}
-          />
-          <button onClick={alterar}>alterar</button>
-        </div>
+        {props.characters?.map((charaters) => (
+          <div className="card">
+            {charaters.image ? (
+              <img src={charaters.image} alt={charaters.name} />
+            ) : (
+              <div className="noImage">imagem indisponivel</div>
+            )
+            }
+            <h2>{charaters.name}</h2>
+            <p>
+              <strong>casa</strong>{charaters.house || "Desconhecido"}
+            </p>
+          </div>
+        )
 
-        <div className="calculadera">
-          <input type="text"
-            value={inputN1}
-            onChange={(e) => setInputN1(e.target.value)}
-          />
+        )}
+      </div>
 
-          <button onClick={somas}>+</button>
-          <button onClick={restas}>-</button>
-          <button onClick={multiplicacao}>*</button>
-
-          <input type="text"
-          value={inputN2}
-          onChange={(e) => setInputN2(e.target.value)}
-          />
-        </div>
     </div>
   )
+
 }
 
 export default Body
--- ola/src/components/Header.jsx
+++ ola/src/components/Header.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import './Header.css'
 
 function Header() {
+  const [searchTerm, setSarchTerm] = React.useState('')
   return (
     <div className='Header'>
-        <h1>Dinossauros</h1>
+        <h1>buscador de personagens - harry potter</h1>
+        <input
+         type="text" 
+         placeholder="Digite o nome..."
+         value={searchTerm}
+         onChange={(e) => setSarchTerm(e.target.value)}
+         className='search'
+         />
     </div>
   )
 }
